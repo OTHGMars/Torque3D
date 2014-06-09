50605d1d4495ee483f87b8d319e1885ea8975840
OTHGMars <mmars1149@aol.com>
2014-06-09 14:59:36 -0400

    Mount Process Order Fix 2 Added a check to SceneObject::setProcessTick() to prevent objects that have mounts from being removed from the process list so the processAfter chain of the mounted objects is not broken.

--- Engine/source/scene/sceneObject.cpp
+++ Engine/source/scene/sceneObject.cpp
@@ -944,7 +944,8 @@ void SceneObject::setProcessTick( bool t )
 
    if ( mProcessTick )
    {
-      plUnlink();
+      if ( !getMountedObjectCount() )
+         plUnlink(); // Only unlink if there is nothing mounted to us
       mProcessTick = false;
    }
    else
